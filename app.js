873dc4453a911b023560d7533d2b1949ca6efb13
Thales Alberto <Thales Alberto>
2018-06-15 13:16:08 -0300

    Crud Funcional

---
+++ app.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+var configFirebase = require('./config');
+var firebase = require('firebase');
+var id = express();
+
+var appFirebase = firebase.initializeApp(config);
+var database = appFirebase.database();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+var matchs = database.ref('match');
+
+app.get('/match', function (req, res) {
+
+    console.log("Test")
+    matchs.orderByKey().once("value", valueToMap => {
+        valueToMap = valueToMap.toJSON();
+        res.send(valueToMap);
+
+    })
+})
+
+app.get(['/match', '/:key'], function (req, res) {
+
+    console.log("test")
+    let key = req.params.key;
+    matchs.child(key).once("value", valueToMap => {
+        valueToMap = valueToMap.toJSON();
+        res.send(valueToMap)
+
+    })
+})
+
+
+app.post('/match', function (err, req, res, next
+){
+
+    var relacao = req.body;
+    if (relacao) {
+
+
+        var keyTemp = matchs.push().key;
+        matchs.push(
+            {
+                key: keyTemp,
+                keyOrigem: relacao.keyOrigem,
+                keyDestino: relacao.keyDestino,
+                status: relacao.status,
+            }
+        );
+
+        res.send(relacao);
+
+    } else {
+        res.send(err);
+    }
+});
+
+app.put(['/', '/:key'], function (req, res
+){
+
+    let key = req.params.key;
+    var relacao = req.body;
+    if (relacao) {
+
+        var keyTemp = matchs.push().key;
+        matchs.child(key).set(
+            {
+                key: keyTemp,
+                keyOrigem: relacao.keyOrigem,
+                keyDestino: relacao.keyDestino,
+                status: relacao.status,
+            }
+        );
+
+        res.send(relacao);
+
+    } else {
+        res.send("ERROR" + relacao)
+    }
+
+});
+
+app.delete(['/', '/:key'], function (req, res) {
+
+    let key = req.params.key;
+    matchs.child(key).remove();
+    res.send("Ok")
+
+})
+
+app.listen(8080, function () {
+    console.log('HTPP// 8080')
+})
+
+
+
+
+
+
+
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = config = {
+    apiKey: "AIzaSyC5VwM-bIPeHR-C1GEIKRQ2un7GX1Awb5Y",
+    authDomain: "fir-tinder.firebaseapp.com",
+    databaseURL: "https://fir-tinder.firebaseio.com",
+    projectId: "fir-tinder",
+    storageBucket: "fir-tinder.appspot.com",
+    messagingSenderId: "130931611492"
+  };
